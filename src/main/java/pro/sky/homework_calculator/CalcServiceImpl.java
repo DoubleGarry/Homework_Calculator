76812bf4de670aec99e6bde717b45b5a10b27093
semenikhin <garry@nextmail.ru>
2023-03-16 22:28:12 +0300

    Comments corrected again

--- src/main/java/pro/sky/homework_calculator/CalcServiceImpl.java
+++ src/main/java/pro/sky/homework_calculator/CalcServiceImpl.java
@@ -8,16 +8,23 @@ public class CalcServiceImpl implements CalcService {
         return "<b>Добро пожаловать в калькулятор</b>";
     }
 
-    public int addition( Integer num1,  Integer num2) {
-        return  num1 + num2;
+    public int addition(Integer num1, Integer num2) {
+        return num1 + num2;
     }
+
     public int subtraction(Integer num1, Integer num2) {
         return num1 - num2;
     }
+
     public int multiplication(Integer num1, Integer num2) {
         return num1 * num2;
     }
+
     public int division(Integer num1, Integer num2) {
-        return num1 / num2;
+        if (num1 == 0 || num2 == 0) {
+            return Integer.parseInt("На ноль делить нельзя");
+        } else {
+            return (num1 / num2);
+        }
     }
 }
